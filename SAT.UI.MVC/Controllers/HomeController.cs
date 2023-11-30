57b566353cef512e3b984992f843ef1b5a2f7d33
Chris Randall <chris.crandall@outlook.com>
2023-11-30 10:16:04 -0600

    Added Contact Functionality

--- SAT.UI.MVC/Controllers/HomeController.cs
+++ SAT.UI.MVC/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SAT.DATA.EF.Models;
 using System.Diagnostics;
+using System.Net.Mail;
+using SAT.UI.MVC.Models;
+using MimeKit;// Added for access to MimeMessage class
+using MailKit.Net.Smtp;// Added for access to SmtpClient class
+
 
 namespace SAT.UI.MVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _Configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _Configuration = config;
         }
 
         public IActionResult Index()
@@ -23,12 +30,72 @@ namespace SAT.UI.MVC.Controllers
             return View();
         }
 
-        public IActionResult Contact ()
+        public IActionResult Contact()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Send email if valid
+                string message = $"You have received a new email from your site's contact form!<br/>" +
+                $"Sender: {model.FirstName} {model.LastName}<br/>Email: {model.Email}<br/>Subject: {model.Subject}<br/>" +
+                $"Message: {model.Message}";
 
+                var Msg = new MimeMessage();
+                Msg.From.Add(new MailboxAddress("No Reply", _Configuration.GetValue<string>("Credentials:Email:User")));
+
+                Msg.To.Add(new MailboxAddress("Personal", _Configuration.GetValue<string>("Credentials:Email:Recipient")));
+
+                Msg.Subject = model.Subject;
+                Msg.Body = new TextPart("HTML") { Text = message };
+
+                Msg.Priority = MessagePriority.Urgent;
+
+                Msg.ReplyTo.Add(new MailboxAddress(model.FirstName + " " + model.LastName, model.Email));
+
+                using (var Client = new MailKit.Net.Smtp.SmtpClient())
+                {
+                    Client.Connect(_Configuration.GetValue<string>("Credentials:Email:Client"));
+
+                    Client.Authenticate(_Configuration.GetValue<string>("Credentials:Email:User"),
+                                        _Configuration.GetValue<string>("Credentials:Email:Password"));
+
+                    // It's possible the mail server could be down when the user tries to contact us,
+                    // so we can encapsulate our code to send the message in a try/catch.
+                    try
+                    {
+                        Client.Send(Msg);
+                    }
+                    catch (Exception ex)
+                    {
+
+                        // If there is an issue we can store an error message
+                        // in a ViewBag variable to be displayed in the View.
+                        ViewBag.ErrorMessage = $"There was an error processing your request." +
+                            $"Please try again later.<br/>" +
+                            $"Error Message: {ex.StackTrace}";
+
+                        // Return the user to the View with their form info intact.
+                        return View(model);
+                    }
+                }
+
+
+                return RedirectToAction(nameof(ThankYou));
+
+            }
+
+            return View(model);
+        }
+
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ SAT.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SAT.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "An Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please include a Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please include a Message is required")]
+        [StringLength(2000)]
+        public string Message { get; set; }
+
+    }
+}
